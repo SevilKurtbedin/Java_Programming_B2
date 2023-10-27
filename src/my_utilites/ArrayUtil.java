1252d1bd74e0a29a87c65ef7a40360caffa72042
SevilKurtbedin <isevil7777@gmail.com>
2023-10-26 22:21:41 -0400

    Commit - Day 26. More methods info

--- src/my_utilites/ArrayUtil.java
+++ src/my_utilites/ArrayUtil.java
@@ -1,4 +1,37 @@
 package my_utilites;
 
 public class ArrayUtil {
+    /**
+     * This method calculate summary Arrays numbers what we write in input.
+     * @return Summary of all numbers.
+     */
+    public static int sumOfArray (int [] arr) {
+        int sum = 0;
+        for (int eachNum : arr) {
+            sum += eachNum;
+        }
+        return sum;
+    }
+    public static int maxInArray (int [] arr) {
+        int max = arr [0];
+        for (int eachElement : arr) {
+            if (eachElement > max) {
+                max = eachElement;
+            }
+        }
+        return max;
+    }
+    /**
+     * This method accepts an int array and an int numb
+     * Then check if the number is in the array
+     * If yes, return true, otherwise return false
+     */
+    public static boolean containsInArray (int [] arr, int num) {
+        for (int eachElement : arr) {
+            if (eachElement == num) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
