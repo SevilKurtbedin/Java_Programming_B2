33a09be1e27973c470dd971239216b1e8acf25c4
SevilKurtbedin <isevil7777@gmail.com>
2023-11-21 23:40:22 -0500

    Commit - Day 37 11.21.2023 - Java - Object-Oriented Programming (OOP) new topics abstract class

---
+++ src/Day_37_a_abstraction/cars/ElectricCar.java
@@ -0,0 +1,5 @@
+package Day_37_a_abstraction.cars;
+
+public abstract class ElectricCar extends Car{
+    public abstract void charge();
+}
---
+++ src/Day_37_a_abstraction/cars/Tesla.java
@@ -0,0 +1,17 @@
+package Day_37_a_abstraction.cars;
+
+public class Tesla extends ElectricCar {
+
+
+    @Override
+    public void charge(){
+        System.out.println("Charging tesla");
+    }
+
+    @Override
+    public void start(){
+        System.out.println("Start remotely");
+    }
+
+
+}
