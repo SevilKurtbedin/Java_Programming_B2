ba9d1614bceba22c59f3f53bf852abae12925048
SevilKurtbedin <isevil7777@gmail.com>
2023-12-06 22:10:17 -0500

    Commit - Day 43 Day 43 - Sut - 12.06.2023 - Java - Map_recap Enum intro

---
+++ src/Day_43_map/enum_intro/Browser.java
@@ -0,0 +1,13 @@
+package Day_43_map.enum_intro;
+
+// enum --- > Enumeration
+// It is a special datatype that is MOSTLY used to define the CONSTANTS
+// Usually written in UPPERCASE
+// They act like class.So, they can have variable (MOSTLY Used for variables)and methods
+
+public enum Browser {
+
+    CHROME, FIREFOX, SAFARI, EDGE, OPERA
+
+    // public final static int NUMBER = 10;
+}
